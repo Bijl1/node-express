971c830b351d0370063240a3c457c95ebb692867
Jonathan Bijl <sannoayo@gmail.com>
2023-06-04 22:10:27 -0400

    update

---
+++ User.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        id: 1,
+        name: 'Lou',
+        email: 'lou@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Noah',
+        email: 'noah@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Evy',
+        email: 'Evy@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Sterling',
+        email: 'sterling@gmail.com'
+    },
+]
+
+module.exports = users;
