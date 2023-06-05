39685068ca91c33b0fc58db5710ece8b56c488b7
Jonathan Bijl <sannoayo@gmail.com>
2023-06-04 22:58:34 -0400

    get user by id

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,4 +3,20 @@ const router = express.Router()
 const uuld = require('uuid')
 let users = require('../../Users')
 
-router.get
+//get all users
+router.get('/', (req,res) => {
+    res.json(users);
+});
+
+//get user by id
+router.get('/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id))
+
+    if (found) {
+        res.json(users.filter(user => user.id === parseInt(req.params.id)));
+    } else {
+        res.sendStatus(400);
+    }
+})
+
+module.exports = router
